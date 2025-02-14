39500f71a612debca93714a946518d95cdaa716f
Vedant Mane <mane.v@northeastern.edu>
2025-02-14 02:41:40 -0500

    Updated FastAPI URL in Streamlit

--- frontend/app.py
+++ frontend/app.py
@@ -7,40 +7,7 @@ load_dotenv()
 
 # Airflow API endpoint
 AIRFLOW_API_URL = "http://localhost:8080"
-
-<<<<<<< HEAD
-st.title("SEC Data - Bridge")
-
-
-# # Input fields for year and quarter
-year = st.selectbox("Select Year",("2024","2023","2022","2021","2020","2019","2018","2017"))
-quarter = st.selectbox("Select Quarter", ("1","2","3","4"))
-
-if st.button("Fetch Data"):
-    # Payload for triggering the DAG
-    payload = {
-        "conf": {
-            "year": year,
-            "quarter": quarter
-        }
-    }
-    dag_id = "sec_data_to_s3_scraper" 
-    AIRFLOW_API_URL = f"http://localhost:8080/api/v1/dags/{dag_id}/dagRuns"
-
-    # Trigger the DAG via Airflow REST API
-    response = requests.post(
-        AIRFLOW_API_URL,
-        json=payload,
-        auth=(f"{AIRFLOW_USER}", f"{AIRFLOW_PASSCODE}")
-    )
-
-    if response.status_code == 200:
-        st.success("DAG triggered successfully!")
-    else:
-        st.error(f"Failed to trigger DAG: {response.text}")
-=======
-QUERY_API_URL = "http://localhost:8000"
->>>>>>> origin/main
+QUERY_API_URL = "https://fastapi-service-7ss2sa6dka-uc.a.run.app"
 
 def populate_airflow_page():
     # Display the airflow page
